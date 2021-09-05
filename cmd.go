2913d6300869a8d63c6f886bc47308ab4ada925f
Viacheslav Poturaev <vearutop@gmail.com>
2021-09-05 16:50:59 +0200

    Load app coonfig from file

--- cmd.go
+++ cmd.go
@@ -17,6 +17,7 @@ import (
 func Start(envPrefix string, cfg WithBaseConfig, init func(docsMode bool) (*BaseLocator, http.Handler)) {
 	ver := flag.Bool("version", false, "Print application version and exit.")
 	docs := flag.Bool("openapi", false, "Print application OpenAPI spec and exit.")
+	confFile := flag.String("conf", "", "Config file with ENV variables to load.")
 	flag.Parse()
 
 	if ver != nil && *ver {
@@ -38,9 +39,7 @@ func Start(envPrefix string, cfg WithBaseConfig, init func(docsMode bool) (*Base
 		return
 	}
 
-	if err := config.Load(envPrefix, cfg); err != nil {
-		log.Fatalf("failed to load config: %v", err)
-	}
+	loadConfig(envPrefix, confFile, cfg)
 
 	loc, router := init(false)
 
@@ -54,5 +53,16 @@ func Start(envPrefix string, cfg WithBaseConfig, init func(docsMode bool) (*Base
 	err = <-loc.Wait()
 	if err != nil {
 		loc.CtxdLogger().Error(context.Background(), err.Error())
+	}
+}
+
+func loadConfig(envPrefix string, conf *string, cfg WithBaseConfig) {
+	var cfgLoaders []func() error
+	if conf != nil && *conf != "" && *conf != ".env" {
+		cfgLoaders = append(cfgLoaders, config.WithEnvFiles(*conf))
+	}
+
+	if err := config.Load(envPrefix, cfg, cfgLoaders...); err != nil {
+		log.Fatalf("failed to load config: %v", err)
 	}
 }
